560e2a424874abfab6bca24f2be4bed1732c673e
EvgLapshin <zhenia.lapshin201@yandex.ru>
2024-12-23 11:28:20 +0500

    Revert "simplify char* allocation".
    This reverts commit 0012090d703ce74769f656cd359570845348e16e.

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,8 @@ int my_merge_main() {
     const char* input1 = "<1:2>,<1:1:3>";
     const char* input2 = "{3:4,4:5,\"seven\"}";
 
+    char* result;
+
     // Инициализация буферов
     if (Bu8map(output, 1UL << 32) || 
         Bu8map(input, 1UL << 32) || 
@@ -78,7 +80,7 @@ int my_merge_main() {
 
         // Преобразуем JDR результат в char*
         size_t resultLength = $len(Bu8cdata(output));
-        char result[resultLength + 1];
+        result = (char*)malloc(resultLength + 1);
         strncpy(result, (char*)*Bu8cdata(output), resultLength);
         result[resultLength] = '\0';
 
@@ -97,6 +99,8 @@ int my_merge_main() {
     Bu8unmap(input);
     Bu8unmap(mergeBuf);
     B$u8cunmap(ins);
+
+    free(result);
 
     return 0;
 }
